afe3c4404c7a050424c12fc93ed88b94dcbc5956
dtanphat9388 <dtanphat9388@gmail.com>
2018-11-24 23:43:56 +0700

    remove miss file

--- server.js
+++ server.js
@@ -10,8 +10,6 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
-require('./passportLocal.config')
-
 app.use('/', express.static('./public'))
 
 /* public route */
