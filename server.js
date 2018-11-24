9fc5982c5354f40ac39d9e663828b869ff4ebca1
dtanphat9388 <dtanphat9388@gmail.com>
2018-11-24 23:42:32 +0700

    remove passport & add jwt

--- server.js
+++ server.js
@@ -1,44 +1,54 @@
 const express = require('express');
-const session = require('express-session');
-const passport = require('passport')
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
 
 require('./db/index');
+const User = mongoose.model('User');
 
 const app = express();
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
-app.use(session({
-  secret: 'fdafa',
-  cookie: {
-    httpOnly: true,
-    maxAge: 1000 * 60 * 60 * 24 * 5 //5d
-  }
-}))
-app.use(passport.initialize())
-app.use(passport.session())
+
 require('./passportLocal.config')
 
 app.use('/', express.static('./public'))
 
 /* public route */
-app.post('/auth/register', (req, res) => {res.send('fdsafsd')})
-
-app.post('/auth/login', 
-  passport.authenticate('local', {
-    failureRedirect: '/auth/login',
-    successRedirect: '/auth/me'
-  }),
-  (req, res) => {
-    res.send('logined')
+app.post('/auth/register', (req, res) => {
+  const {username, password} = req.body;
+  if (!username || !password) return res.send({msg: 'missing username or password'})
+  User.findOne({username}).then( user => {
+    if (user) return res.send({msg: `User was registered!`})
+    User.create({username, password}).then( user => {
+      let token = jwt.sign({ username }, 'hackathon');
+      res.send({token, user})
+    })
   })
+})
 
+app.post('/auth/login', (req, res) => {
+  const {username, password} = req.body;
+  if (!username || !password) return res.send({msg: 'missing username or password'})
+  User.findOne({username}).then(user => {
+    if (!user) return res.send({msg: 'User not exist!'})
+    if (user.password !== password) return res.send({msg: 'Password not match!'})
+    let token = jwt.sign({ username }, 'hackathon');
+    res.send({token, user})
+  })
+})
 
 
 /* private route */
 app.use((req, res, next) => {
-  if (req.isAuthenticated()) return next();
-  res.redirect('/auth/login')
+  const { token } = req.query;
+  if (!token) return res.send({msg: 'Not yet Authorized!'})
+  jwt.verify(token, 'hackathon', function(err, decoded) {
+    User.findOne({username: decoded.username}).then(user => {
+      req.user = user;
+      next()
+    })
+  });
 })
 
 app.get('/auth/me', (req, res) => {
