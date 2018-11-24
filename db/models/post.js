fe1adba439558300f36f1d3ec2e9189b0d8e48bb
dtanphat9388 <dtanphat9388@gmail.com>
2018-11-25 01:08:20 +0700

    update model schema

--- db/models/post.js
+++ db/models/post.js
@@ -1,7 +1,23 @@
 const mongoose = require('mongoose')
 
+const PositionSchema = mongoose.Schema({
+  x: Number,
+  y: Number,
+})
+
+const highlightSchema = mongoose.Schema({
+  user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+  position: [PositionSchema],
+  range: [Number],
+  content: String
+})
+
 const schema = mongoose.Schema({
-  name: String
+  title: String,
+  description: String,
+  content: Object,
+  highlights: [highlightSchema]
+
 })
 
 module.exports = mongoose.model("Post", schema)
--- db/models/user.js
+++ db/models/user.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const schema = mongoose.Schema({
   password: String,
   username: String,
-  post_id: {type: mongoose.Schema.Types.ObjectId, ref: 'Post'}
+  notebookIDs: [{type: mongoose.Schema.Types.ObjectId, ref: 'Post'}]
 })
 
 module.exports = mongoose.model("User", schema)
